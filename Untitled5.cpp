9790abc1607820b9a4e015e4e506cf0584c67a37
dungnguyen1704 <dungct1704@gmail.com>
2025-04-20 21:08:09 +0700

    Add files via upload

---
+++ Untitled5.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+	int main(){
+	int n = 20 ;
+	for (int i = 0; i <= n; i++){
+	
+		if(i % 2 == 0){
+			printf("	%d",i);
+		}
+		else{
+			
+		}
+	}
+return 0;
+	}
+
+
+
